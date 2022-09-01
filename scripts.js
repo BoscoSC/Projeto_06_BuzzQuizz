be76a7a2e395a323d64c097f67d6675b7a764a86
gamabarbara <barbara.cp@outlook.com>
2022-09-01 16:56:31 -0300

    Verificação dos inputs

--- scripts.js
+++ scripts.js
@@ -1,32 +1,32 @@
-function tela3pt1(elemento){
-    const paiDoElemento = elemento.parentNode
-    const voDoElemento = paiDoElemento.parentNode
-    const taraVoDoElemento = voDoElemento.parentNode
-    taraVoDoElemento.innerHTML = `<div class="comecePeloComeco">
+function tela3pt1(){
+    const elemento = document.querySelector('.conteudo');
+    elemento.innerHTML = `<div class="comecePeloComeco">
     Comece pelo começo
 </div>
 <div class="informacoesBasicas">
     <div class="tituloCriarQuizz caixaDeInputTela3">
-        <input id=”placeholder-text” type="text" placeholder="Título do seu quizz">
+        <input class="input1" id=”placeholder-text” type="text" placeholder="Título do seu quizz">
     </div>
     <div class="urlCriarQuizz caixaDeInputTela3">
-        <input type="text" placeholder="URL da imagem do seu quizz">
+        <input class="input2" type="text" placeholder="URL da imagem do seu quizz">
     </div>
     <div class="qtdePerguntasCriarQuizz caixaDeInputTela3">
-        <input type="text" placeholder="Quantidade de perguntas do quizz">
+        <input class="input3" type="text" placeholder="Quantidade de perguntas do quizz">
     </div>
     <div class="qtdeNiveisCriarQuizz caixaDeInputTela3">
-        <input type="text" placeholder="Quantidade de níveis do quiz">
+        <input class="input4" type="text" placeholder="Quantidade de níveis do quiz">
     </div>
 </div>
-<div class="botaoProsseguir" onclick="tela3pt2(this)">
+<div class="botao">
+<button class="botaoProsseguir" onclick="verificarInformacoes()">
     Prosseguir para criar perguntas
-</div>'`
+</button>'
+</div>`
 } 
 
-function tela3pt2(elemento){
-    const paiDoElemento = elemento.parentNode
-    paiDoElemento.innerHTML = `
+function tela3pt2(){
+    const elemento = document.querySelector('.comecePeloComeco');
+    elemento.innerHTML = `
     <div class ="conteudoTela3pt2">
     <div class="comecePeloComeco">
         Crie suas perguntas
@@ -95,18 +95,19 @@ function tela3pt2(elemento){
             
     </div>
     <br/>
-
-    <div class="botaoProsseguir" onclick="tela3pt3(this)">
+<div class="botao">
+    <button class="botaoProsseguir"  onclick="tela3pt3(this)">
         Prosseguir para criar níveis
+    </button>
     </div>
     
 </div>
 <br/><br/><br/><br/>`
 }
 
-function tela3pt3(elemento){
-    const paiDoElemento = elemento.parentNode
-    paiDoElemento.innerHTML = `<div class="conteudoTela3pt3">
+function tela3pt3(){
+    const elemento = document.querySelector('.conteudoTela3pt2');
+    elemento.innerHTML = `<div class="conteudoTela3pt3">
     <div class="comecePeloComeco">
         Agora, decida os níveis!
     </div>
@@ -143,18 +144,19 @@ function tela3pt3(elemento){
     </div>
     <br/>
 
-    
-    <div class="botaoProsseguir" onclick="tela3pt4(this)">
+    <div class="botao">
+    <button class="botaoProsseguir" onclick="tela3pt4(this)">
         Finalizar Quizz
-    </div>
+    </button>
+    <
     
     <br/><br/><br/><br/>
 </div>`
 }
 
-function tela3pt4(elemento){
-    const paiDoElemento = elemento.parentNode
-    paiDoElemento.innerHTML = `<div class="conteudoTelapt4">
+function tela3pt4(){
+    const elemento = document.querySelector('.conteudoTela3pt3')
+    elemento.innerHTML = `<div class="conteudoTelapt4">
     <div class="seuQuizzEstaPronto">
         Seu quizz está pronto!
     </div>
@@ -172,4 +174,35 @@ function tela3pt4(elemento){
        
     <br/><br/><br/><br/>
 </div>`
+}
+
+let urlInput;
+
+function verificarInformacoes() {
+  const inputTitulo = document.querySelector(".input1").value;
+  const qtdePerguntasInput = document.querySelector(".input3").value;
+  const qtdeNiveisInput = document.querySelector(".input4").value;
+    const botao = document.querySelector(".botaoProsseguir");
+  if (
+    (inputTitulo.length < 20 ||
+    inputTitulo.length > 65) ||
+    checkUrl() ||
+    qtdePerguntasInput < 3 ||
+    qtdeNiveisInput < 2
+  ) {
+    alert("Insira as informações corretas");
+    
+  } else {
+    tela3pt2();
+    
+  }
+}
+
+function checkUrl(string) {
+  try {
+    urlInput = new URL(string);
+    console.log("Valid URL!");
+  } catch (err) {
+    console.log("Invalid URL!");
+  }
 }
